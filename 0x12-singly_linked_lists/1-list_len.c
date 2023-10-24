e33da0aaf20eda799e9642eca5cba5efee763deb
BhekumusaEric <kelvinbacela@gmail.com>
2023-10-24 09:27:23 -0700

    Lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,19 +2,19 @@
 #include "lists.h"
 
 /**
- * list_len - this function returns the number of elements in a linked list...
- * @h: a pointer to the list_t list....
+ * list_len - this function returns the number of elements in a linked list.....
+ * @h: pointer to the list_t list.....
  *
- * Return: number of elements in h ,
+ * Return: number of elements in h .....
  */
 size_t list_len(const list_t *h)
 {
- size_t n = 0;
+	size_t m = 0;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+	while (h)
+	{
+		m++;
+		h = h->next;
+	}
+	return (m);
 }
--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,11 +3,11 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - it  prints a sentence before the main......
- * function is executed......
+ * first - prints a sentence before the main function
+ * function is executed yes ...
  */
 void first(void)
 {
- printf("You're beat! and yet, you must allow,\n");
- printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
